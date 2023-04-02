6cae950a156f6c115427fb5a193bf0e266f80c1d
jmaurojr <112908979+jmaurojr@users.noreply.github.com>
2023-04-02 20:12:11 -0300

    add cards

--- js/game.js
+++ js/game.js
@@ -1,6 +1,17 @@
 const grid = document.querySelector(".grid");
 
-const cards = [""];
+const characters = [
+  "1trio",
+  "2trio",
+  "3trio",
+  "4trio",
+  "5trio",
+  "6trio",
+  "7trio",
+  "AF",
+  "GL",
+  "NP",
+];
 
 const createElement = (tag, className) => {
   const element = document.createElement(tag);
@@ -8,15 +19,23 @@ const createElement = (tag, className) => {
   return element;
 };
 
-const createCard = () => {
+const createCard = (character) => {
   const card = createElement("div", "card");
   const front = createElement("div", "face front");
   const back = createElement("div", "face back");
 
+  front.style.backgroundImage = `url('../img/${character}.jpg')`;
   card.appendChild(front);
   card.appendChild(back);
 
   return card;
 };
 
-createCard();
+const loadGame = () => {
+  characters.forEach((character) => {
+    const card = createCard(character);
+    grid.appendChild(card);
+  });
+};
+
+loadGame();
